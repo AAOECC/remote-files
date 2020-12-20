29cf1c20d9985e80cbfefdbb72a1770f07269f69
aaoecc <aaoecc165@gmail.com>
2020-12-20 22:45:42 +0800

    1220

--- OnJava8-src/OnJava8-Examples-master/innerclasses/Parcel10.java
+++ OnJava8-src/OnJava8-Examples-master/innerclasses/Parcel10.java
@@ -6,26 +6,30 @@
 // construction on an anonymous inner class
 
 public class Parcel10 {
-  public Destination
-  destination(final String dest, final float price) {
+  public Destination destination(final String dest, final float price) {
     return new Destination() {
       private int cost;
       // Instance initialization for each object:
       {
         cost = Math.round(price);
-        if(cost > 100)
-          System.out.println("Over budget!");
+        if (cost > 100)
+          System.out.println("Over budget!" + cost);
       }
       private String label = dest;
+
       @Override
-      public String readLabel() { return label; }
+      public String readLabel() {
+        return label;
+      }
     };
   }
+
   public static void main(String[] args) {
     Parcel10 p = new Parcel10();
     Destination d = p.destination("Tasmania", 101.395F);
+    System.out.println(d.readLabel());
   }
 }
-/* Output:
-Over budget!
-*/
+/*
+ * Output: Over budget!
+ */
