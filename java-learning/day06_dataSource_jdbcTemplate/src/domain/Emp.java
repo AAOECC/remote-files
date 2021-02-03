b9b9a610b5ca6ab7745ac609a2378c9cc8066edd
aaoecc <aaoecc165@gmail.com>
2021-02-03 17:21:27 +0800

    html

--- java-learning/day06_dataSource_jdbcTemplate/src/domain/Emp.java
+++ java-learning/day06_dataSource_jdbcTemplate/src/domain/Emp.java
@@ -6,20 +6,20 @@ import java.util.Date;
  * 数据库emp表实体类
  */
 public class Emp {
-    private int id;
+    private Integer id;
     private String eName;
-    private int job_id;
-    private int mgr;
+    private Integer job_id;
+    private Integer mgr;
     private Date joinDate;
-    private double salary;
-    private double bonus;
-    private int dept_id;
+    private Double salary;
+    private Double bonus;
+    private Integer dept_id;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -31,19 +31,19 @@ public class Emp {
         this.eName = eName;
     }
 
-    public int getJob_id() {
+    public Integer getJob_id() {
         return job_id;
     }
 
-    public void setJob_id(int job_id) {
+    public void setJob_id(Integer job_id) {
         this.job_id = job_id;
     }
 
-    public int getMgr() {
+    public Integer getMgr() {
         return mgr;
     }
 
-    public void setMgr(int mgr) {
+    public void setMgr(Integer mgr) {
         this.mgr = mgr;
     }
 
@@ -55,38 +55,27 @@ public class Emp {
         this.joinDate = joinDate;
     }
 
-    public double getSalary() {
+    public Double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
+    public void setSalary(Double salary) {
         this.salary = salary;
     }
 
-    public double getBonus() {
+    public Double getBonus() {
         return bonus;
     }
 
-    public void setBonus(double bonus) {
+    public void setBonus(Double bonus) {
         this.bonus = bonus;
     }
 
-    public int getDept_id() {
+    public Integer getDept_id() {
         return dept_id;
     }
 
-    public void setDept_id(int dept_id) {
-        this.dept_id = dept_id;
-    }
-
-    public Emp(int id, String eName, int job_id, int mgr, Date joinDate, double salary, double bonus, int dept_id) {
-        this.id = id;
-        this.eName = eName;
-        this.job_id = job_id;
-        this.mgr = mgr;
-        this.joinDate = joinDate;
-        this.salary = salary;
-        this.bonus = bonus;
+    public void setDept_id(Integer dept_id) {
         this.dept_id = dept_id;
     }
 
--- java-learning/day06_dataSource_jdbcTemplate/src/jdbcTemplaste/JdbcTemplateDemo2.java
+++ java-learning/day06_dataSource_jdbcTemplate/src/jdbcTemplaste/JdbcTemplateDemo2.java
@@ -2,9 +2,14 @@ package jdbcTemplaste;
 
 import static org.junit.Assert.assertEquals;
 
+import domain.Emp;
 import org.junit.Test;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import utils.JDBCUtils;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  *  1. 修改1号数据的 salary 为 10000
@@ -17,13 +22,14 @@ import utils.JDBCUtils;
  */
 public class JdbcTemplateDemo2 {
 
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
 
     /**
      * 1. 修改1号数据的 salary 为 10000
      */
     @Test
     public void test1(){
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
+        //JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
 
         String sql = "update emp set salary=10000 where id = 1001";
 
@@ -36,9 +42,63 @@ public class JdbcTemplateDemo2 {
      */
     @Test
     public void test2(){
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
-        String sql = "insert into emp value";
+        //JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
+        String sql = "insert into emp (id, ename,job_id, dept_id) value (1015, 'zhaoyun',3, 30)";
         int count = jdbcTemplate.update(sql);
         assertEquals(count, 1);
+    }
+
+    /**
+     * 3. 删除刚才添加的记录
+     */
+    @Test
+    public void test3(){
+        String sql = "delete from emp where id = ?";
+        int count = jdbcTemplate.update(sql, 1015);
+        assertEquals(count, 1);
+    }
+
+    /**
+     * 4. 查询id为1001的记录，将其封装为Map集合
+     */
+    @Test
+    public void Test4(){
+        String sql = "select * from emp where id = ?";
+        Map<String, Object> empMap = jdbcTemplate.queryForMap(sql, 1001);
+        System.out.println(empMap);
+    }
+
+    /**
+     * 5. 查询所有记录，将其封装为List
+     */
+    @Test
+    public void Test5(){
+        String sql = "select * from emp";
+        List<Map<String, Object>> mapList = jdbcTemplate.queryForList(sql);
+        for (Map<String, Object> stringObjectMap : mapList) {
+            System.out.println(stringObjectMap);
+        }
+    }
+
+    /**
+     * 6. 查询所有记录，将其封装为Emp对象的List集合
+     */
+    @Test
+    public void Test6(){
+        String sql = "select * from emp";
+        List<Emp> empList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<Emp>(Emp.class));
+        for (Emp emp : empList) {
+            System.out.println(emp);
+        }
+    }
+
+    /**
+     * 7. 查询总记录数
+     */
+    @Test
+    public void test7(){
+        String sql = "select count(id) from emp";
+        Long total = jdbcTemplate.queryForObject(sql, Long.class);
+        System.out.println(total);
     }
 }
